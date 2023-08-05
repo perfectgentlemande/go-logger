8f0d66acfeee65bdbd3e5c69732bfa8b99acbadb
Артем Чаадаев <perfectgentleman.de@ya.ru>
2023-08-05 19:23:42 +0400

    fx zerolog

--- zerolog.go
+++ zerolog.go
@@ -45,22 +45,22 @@ func newZerolog(config *Config) Logger {
 }
 
 func (zw *zerologWrapper) Panic(msg string) {
-	zw.log.Panic().Msg(msg)
+	zw.log.Panic().Fields(zw.fields).Msg(msg)
 }
 func (zw *zerologWrapper) Fatal(msg string) {
-	zw.log.Fatal().Msg(msg)
+	zw.log.Fatal().Fields(zw.fields).Msg(msg)
 }
 func (zw *zerologWrapper) Error(msg string) {
-	zw.log.Error().Msg(msg)
+	zw.log.Error().Fields(zw.fields).Msg(msg)
 }
 func (zw *zerologWrapper) Warning(msg string) {
-	zw.log.Warn().Msg(msg)
+	zw.log.Warn().Fields(zw.fields).Msg(msg)
 }
 func (zw *zerologWrapper) Info(msg string) {
-	zw.log.Info().Msg(msg)
+	zw.log.Info().Fields(zw.fields).Msg(msg)
 }
 func (zw *zerologWrapper) Debug(msg string) {
-	zw.log.Debug().Msg(msg)
+	zw.log.Debug().Fields(zw.fields).Msg(msg)
 }
 
 func copyFields(fields Fields) Fields {
