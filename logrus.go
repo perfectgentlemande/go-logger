77713537df3605169fd7c8cada48d80595d18d4d
Артем Чаадаев <perfectgentleman.de@ya.ru>
2023-08-05 19:32:59 +0400

    added logrus

--- logrus.go
+++ logrus.go
@@ -12,18 +12,36 @@ func newLogrus(config *Config) Logger {
 	return &logrusWrapper{}
 }
 
-func (lw *logrusWrapper) Debug(msg string) {
-	lw.log.Debug(msg)
+func (lw *logrusWrapper) Panic(msg string) {
+	lw.log.Panic(msg)
 }
-func (lw *logrusWrapper) Info(msg string) {
-	lw.log.Info(msg)
-}
-func (lw *logrusWrapper) Warning(msg string) {
-	lw.log.Warning(msg)
+func (lw *logrusWrapper) Fatal(msg string) {
+	lw.log.Fatal(msg)
 }
 func (lw *logrusWrapper) Error(msg string) {
 	lw.log.Error(msg)
 }
-func (lw *logrusWrapper) Fatal(msg string) {
-	lw.log.Fatal(msg)
+func (lw *logrusWrapper) Warning(msg string) {
+	lw.log.Warning(msg)
+}
+func (lw *logrusWrapper) Info(msg string) {
+	lw.log.Info(msg)
+}
+func (lw *logrusWrapper) Debug(msg string) {
+	lw.log.Debug(msg)
+}
+func (lw *logrusWrapper) WithField(key string, value interface{}) Logger {
+	return &logrusWrapper{
+		log: lw.log.WithField(key, value),
+	}
+}
+func (lw *logrusWrapper) WithFields(fields Fields) Logger {
+	return &logrusWrapper{
+		log: lw.log.WithFields(logrus.Fields(fields)),
+	}
+}
+func (lw *logrusWrapper) WithError(err error) Logger {
+	return &logrusWrapper{
+		log: lw.log.WithError(err),
+	}
 }
